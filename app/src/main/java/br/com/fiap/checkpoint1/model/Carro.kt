50d40c803ead061979372ca82e139c14e4fb4f97
Lucas <lukinha11x@gmail.com>
2024-04-02 15:53:38 -0300

    feat(Carro.kt): adding Java Doc

--- app/src/main/java/br/com/fiap/checkpoint1/model/Carro.kt
+++ app/src/main/java/br/com/fiap/checkpoint1/model/Carro.kt
@@ -2,6 +2,14 @@ package br.com.fiap.checkpoint1.model
 
 import java.math.BigDecimal
 
+/**
+ * Representa um objeto Carro que possui informações como nome, descrição, marca, valor e URL da imagem.
+ * @property nome: O nome do carro.
+ * @property descricao: A descrição do carro.
+ * @property marca: A marca do carro.
+ * @property valor: O valor do carro em BigDecimal.
+ * @property imageUrl: A URL da imagem do carro.
+ */
 class Carro (
     val nome: String,
     val descricao: String,
