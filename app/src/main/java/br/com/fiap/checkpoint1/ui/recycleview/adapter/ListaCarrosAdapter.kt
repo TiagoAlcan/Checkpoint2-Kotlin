4377d172e74a36220a556cbc0f58603b29910a71
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-03 18:20:19 -0300

    Coloncando java doc nos documentos que não tinham

--- app/src/main/java/br/com/fiap/checkpoint1/ui/recycleview/adapter/ListaCarrosAdapter.kt
+++ app/src/main/java/br/com/fiap/checkpoint1/ui/recycleview/adapter/ListaCarrosAdapter.kt
@@ -1,5 +1,6 @@
-package br.com.fiap.checkpoint1.ui.recycleview.adapter
+package br.com.fiap.checkpoint1.ui.recycleview.adapter // Define o pacote onde a classe está localizada
 
+// Importações de bibliotecas e classes necessárias
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -9,13 +10,16 @@ import androidx.recyclerview.widget.RecyclerView
 import br.com.fiap.checkpoint1.R
 import br.com.fiap.checkpoint1.model.Carro
 
+// Define a classe ListaCarrosAdapter que herda de RecyclerView.Adapter
 class ListaCarrosAdapter(
-    private val context: Context,
-    private val carros: List<Carro>
+    private val context: Context, // Contexto para inflar o layout
+    private val carros: List<Carro> // Lista de carros a serem exibidos
 ) : RecyclerView.Adapter<ListaCarrosAdapter.ViewHolder>() {
 
+    // Define a classe interna ViewHolder para gerenciar a exibição de cada item
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
+        // Método para vincular os dados de um carro ao layout do item
         fun vincula(carro: Carro) {
             val nome = itemView.findViewById<TextView>(R.id.nome)
             nome.text = carro.nome
@@ -34,6 +38,7 @@ class ListaCarrosAdapter(
         }
     }
 
+    // Método para criar um ViewHolder e inflar o layout do item
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
@@ -43,12 +48,12 @@ class ListaCarrosAdapter(
         return ListaCarrosAdapter.ViewHolder(view)
     }
 
-
+    // Método para retornar a quantidade de itens na lista
     override fun getItemCount(): Int = carros.size
 
+    // Método para vincular os dados de um carro ao ViewHolder na posição especificada
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val carro = carros[position]
         holder.vincula(carro)
     }
-
 }
